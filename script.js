97d2a804a10c7d36deddd31eb6b4539375816e43
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-11 17:25:44 -0300

    Terceiro Requisito

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const inputEmail = document.getElementById('input-email');
+const inputSenha = document.getElementById('input-senha');
+const buttonLogin = document.getElementById('button-login');
 
+function login() {
+  if (inputEmail.value === "tryber@teste.com" || inputSenha.value === "123456") {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+buttonLogin.addEventListener('click', login);
